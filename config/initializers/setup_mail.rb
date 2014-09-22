1ce25c70256b3d0fb4cb777c59437c2c9a2c21e0
“Jen <mpjeno@gmail.com>
2014-09-22 15:42:51 -0500

    Switched back to local host for the domain

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address        => "smtp.gmail.com",
   :port           => 587,
-  :doman          => "http://peaceful-lowlands-7052.herokuapp.com/",
+  :domain          => "localhost:3000",
   :user_name      => ENV['EMAIL'],
   :password       => ENV['EMAIL_PASSWORD'],
   :authentication => "plain",
