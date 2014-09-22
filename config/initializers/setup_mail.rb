d6caa521437eb0a968e1fb12bfd3632e8a1f0a11
“Jen <mpjeno@gmail.com>
2014-09-22 15:40:25 -0500

    Changed domain to "http://peaceful-lowlands-7052.herokuapp.com/"

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address        => "smtp.gmail.com",
   :port           => 587,
-  :doman          => "localhost:3000",
+  :doman          => "http://peaceful-lowlands-7052.herokuapp.com/",
   :user_name      => ENV['EMAIL'],
   :password       => ENV['EMAIL_PASSWORD'],
   :authentication => "plain",
